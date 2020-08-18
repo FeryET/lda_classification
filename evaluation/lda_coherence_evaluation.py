085ec2801da012e093bdf765abb7ebe761c09da7
FeryET <farhoodetaati@ut.ac.ir>
2020-08-18 11:28:14 +0430

    adds a coherence evaluation pipeline

---
+++ evaluation/lda_coherence_evaluation.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from model.gensim_lda_vectorizer import GensimLDAVectorizer
+
+
+class LDACoherenceEvaluator:
+    def __init__(self, min_topic, max_topic, step, **kwargs):
+        self.params = kwargs
+        self.topic_range = list(range(min_topic, max_topic, step))
+
+    def evaluate(self, docs, coherence_method="c_v", plot=True, ax=None,
+                 mark_max=False, grid=True):
+        coherence_scores = []
+        for n in tqdm(self.topic_range, desc="computing coherences"):
+            vectorizer = GensimLDAVectorizer(num_topics=n, **self.params).fit(
+                    docs)
+            c = vectorizer.evaluate_coherence(docs,
+                                              coherence=coherence_method).get_coherence()
+            coherence_scores.append(c)
+        if plot is True:
+            if ax is None:
+                ax = plt.subplot()
+            ax.plot(self.topic_range, coherence_scores, label="coherence",
+                    marker=".")
+            ax.set_xticks(self.topic_range)
+            ax.set_xlabel("Number of Topics")
+            ax.set_ylabel("Coherence Values")
+            ax.set_title("Evaluation of LDA Model  (metric = {})".format(
+                    coherence_method))
+            if grid is True:
+                ax.grid()
+            if mark_max is True:
+                idx = coherence_scores.index(max(coherence_scores))
+                ax.annotate("{:.3f}".format(coherence_scores[idx]),
+                            (self.topic_range[idx], coherence_scores[idx]),
+                            textcoords="offset points",
+                            # how to position the text
+                            xytext=(3, 3), size=8)
+                ax.axvline(self.topic_range[idx], alpha=0.5, color="r",
+                           linestyle="--")
+            else:
+                return coherence_scores
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dataloader import CogSciData, DataReader
 from preprocess.spacy_cleaner import SpacyCleaner
 from preprocess.gensim_cleaner import GensimCleaner
+from model.gensim_lda_vectorizer import GensimLDAVectorizer
+from evaluation.lda_coherence_evaluation import LDACoherenceEvaluator
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -8,11 +11,15 @@ def main():
     datareader = DataReader(path, data_type=CogSciData)
     df = datareader.to_dataframe()
     texts = list(df["text"])
-    texts = SpacyCleaner().process_documents_multithread(texts)
-    texts = list(df["text"])
     texts = GensimCleaner().process_documents_multithread(texts)
-    df["processed"] = texts
-    print(df)
+    evaluator = LDACoherenceEvaluator(min_topic=2, max_topic=10, step=2,
+                                      alpha="symmetric", beta=None, workers=2,
+                                      lda_iterations=100, return_dense=True,
+                                      max_df=0.5, min_df=5, random_seed=0)
+    ax = evaluator.evaluate(texts, mark_max=True)
+    plt.show()  # vectorizer = GensimLDAVectorizer(5)  # X =
+    # vectorizer.fit_transform(texts)  # print(vectorizer.evaluate_coherence(
+    # texts).get_coherence())
 
 
 if __name__ == '__main__':
