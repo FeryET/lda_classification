e96412c628e2bfcde9d46adedb68d1245765964c
FeryET <farhoodetaati@ut.ac.ir>
2020-08-18 12:46:38 +0430

    tweaks the scripts a bit

--- analysis_scripts/topic_number_analysis.py
+++ analysis_scripts/topic_number_analysis.py
@@ -4,28 +4,27 @@ from preprocess.gensim_cleaner import GensimCleaner
 from preprocess.spacy_cleaner import SpacyCleaner
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from evaluation.lda_coherence_evaluation import LDACoherenceEvaluator
+import argparse
 
 
-def run():
-    path = "/home/farhood/Projects/datasets_of_cognitive/Data/Unprocessed Data"
+def run(path):
     range_n_topics = list(range(2, 51, 6))
     reader = DataReader(path, CogSciData)
     df = reader.to_dataframe()
-    # texts = GensimCleaner().process_documents_multithread(list(df["text"]))
     texts = SpacyCleaner().process_documents_multithread(list(df["text"]))
-    values = []
-    for n in tqdm(range_n_topics):
-        vectorizer = GensimLDAVectorizer(num_topics=n)
-        vectorizer.fit(texts)
-        values.append(vectorizer.evaluate_coherence(texts).get_coherence())
-
-    l1 = plt.plot(range_n_topics, values, label="coherence")
-    # plt.legend((l1), ("coherence"))
-    plt.gca().set_xlabel("Number of Topics")
-    plt.gca().set_ylabel("Coherence Value")
-    plt.title("Number of Topics vs Coherence Values")
+    evaluator = LDACoherenceEvaluator(min_topic=2, max_topic=100, step=10,
+                                      alpha="symmetric", beta=None, workers=4,
+                                      lda_iterations=300, return_dense=True,
+                                      max_df=0.5, min_df=5, random_seed=300)
+    ax = evaluator.evaluate(texts, mark_max=True)
     plt.show()
 
 
 if __name__ == '__main__':
-    run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", action="store",
+                        help="path to the directory of the dataset", type=str,
+                        required=True)
+    path = parser.parse_args().path
+    run(path)
---
+++ evaluation/__init__.py
@@ -0,0 +1,1 @@
+# from evaluation.lda_coherence_evaluation import LDACoherenceEvaluator
