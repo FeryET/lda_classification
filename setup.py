75fc519d08f8ba91e5d2e08639defc87861f2013
FeryET <farhoodetaati@ut.ac.ir>
2020-08-21 02:03:00 +0430

    adds url to setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ import setuptools
 with open("README.md") as fh:
     long_desc = fh.read()
 
-setuptools.setup(name="lda_classification", version="0.0.1",
+setuptools.setup(name="lda_classification", version="0.0.11",
                  author="Farhood Etaati", author_email="farhoodet@gmail.com",
                  long_description=long_desc,
                  long_description_content_type="text/markdown",
                  packages=setuptools.find_packages(),
+                 url="https://github.com/FeryET/lda_classification",
                  classifiers=["Programming Language :: Python :: 3",
                               "License :: OSI Approved :: MIT License",
                               "Operating System :: OS Independent"],
