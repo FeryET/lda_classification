7c83e04d3e4f4a2bbd2a99fbb5e953fc8a950a60
FeryET <farhoodetaati@ut.ac.ir>
2020-08-26 00:58:41 +0430

    Updates example and requirements

--- examples/twentynewsgroup_topic_vis.py
+++ examples/twentynewsgroup_topic_vis.py
@@ -1,24 +1,30 @@
 import logging
+import argparse
+import matplotlib.pyplot as plt
+from gensim.corpora import Dictionary
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from tomotopy import HDPModel
+from matplotlib import cm
+from lda_classification.model import GensimLDAVectorizer, TomotopyLDAVectorizer
+from lda_classification.preprocess.gensim_cleaner import GensimCleaner
 
 import numpy as np
-from gensim.corpora import Dictionary
-from gensim.models.hdpmodel import HdpModel
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from tomotopy import HDPModel
-from lda_classification.model import GensimLDAVectorizer
-from lda_classification.preprocess.gensim_cleaner import GensimCleaner
-from lda_classification.preprocess.spacy_cleaner import SpacyCleaner
-import matplotlib.pyplot as plt
 
 
-# Courtesy of Gensim's documentation
-def plot_difference(mdiff, title="", annotation=None):
-    fig, ax = plt.subplots(figsize=(18, 14))
-    data = ax.imshow(mdiff, cmap='RdBu_r', origin='lower')
-    plt.title(title)
-    plt.colorbar(data)
+def plot_topic_clusters(ax, x2d, y, labels):
+    ax.set_aspect("equal")
+    colors = cm.get_cmap("Spectral", len(labels))
+    for i, l in enumerate(labels):
+        c = colors(i / len(labels))
+        center = x2d.mean(axis=0)
+        ax.scatter(x2d[y == i, 0], x2d[y == i, 1], color=c, label=l, alpha=0.7)
+    ax.grid()
+    ax.legend(prop={'size': 6})
+    ax.autoscale()
+    return ax
 
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
@@ -28,45 +34,77 @@ n_workers = 6
 # Change this to false if you want to search for the
 # number of topics via c_v score (super slow)
 IS_HIERARCHICAL = True
-cats = ["rec.autos", "rec.motorcycles", "rec.sport.baseball",
-        "rec.sport.hockey"]
-data, target = fetch_20newsgroups(subset='all', return_X_y=True,
-                                  categories=cats)
-y_true = LabelEncoder().fit_transform(target)
 
-data_train, data_test, y_train, y_test = train_test_split(data, y_true,
-                                                          test_size=0.1,
-                                                          shuffle=True)
 
-# processor = SpacyCleaner(chunksize=1000)
-processor = GensimCleaner(6)
-docs = processor.transform(data_train)
+def main(use_umap=True):
+    labels = ["rec.autos", "rec.motorcycles", "rec.sport.baseball",
+              "rec.sport.hockey"]
+    data = fetch_20newsgroups(subset='all', categories=labels)
 
-id2word = Dictionary(docs)
-id2word.filter_extremes(no_above=5, no_below=0.5)
-corpus = [id2word.doc2bow(d) for d in docs]
-hdp_model = HDPModel(min_cf=5, rm_top=10)
-for d in docs:
-    hdp_model.add_doc(d)
-hdp_model.burn_in = 100
-hdp_model.train(0)
-for i in range(0, 1000, 10):
-    hdp_model.train(100)
-    print('Iteration: {}\tLog-likelihood: {}\tNum. of topics: {}'.format(i,
-                                                                         hdp_model.ll_per_word,
-                                                                         hdp_model.live_k))
+    data, target = data.data, data.target
+    encoder = LabelEncoder()
+    y_true = encoder.fit_transform(target)
+    data_train, data_test, y_train, y_test = train_test_split(data, y_true,
+                                                              test_size=0.1,
+                                                              shuffle=True)
+    processor = GensimCleaner(6)
+    train_docs = processor.transform(data_train)
+    test_docs = processor.transform(data_test)
+    hdp_model = HDPModel(min_cf=5, rm_top=10)
+    hdp_model.optim_interval = 5
+    for d in train_docs:
+        hdp_model.add_doc(d)
+    hdp_model.burn_in = 100
+    hdp_model.train(0, workers=n_workers)
+    for i in range(0, 1000, 10):
+        hdp_model.train(10, workers=n_workers)
+        print('Iteration: {}\tLog-likelihood: {}\tNum. of topics: {}'.format(i,
+                                                                             hdp_model.ll_per_word,
+                                                                             hdp_model.live_k))
 
-num_of_topics = hdp_model.live_k
-print(num_of_topics)
+    num_of_topics = hdp_model.live_k
+    print(num_of_topics)
 
-vectorizer = GensimLDAVectorizer(num_topics=num_of_topics, max_df=0.5, min_df=5,
-                                 return_dense=False, eval_every=1, passes=50,
-                                 workers=n_workers)
+    vectorizer = TomotopyLDAVectorizer(num_of_topics=num_of_topics,
+                                       workers=n_workers)
 
-X = vectorizer.fit_transform(docs)
+    x_train = vectorizer.fit_transform(train_docs)
+    x_test = vectorizer.transform(test_docs)
 
-mdiff, annotation = vectorizer.lda.diff(vectorizer.lda, distance='jaccard',
-                                        num_words=50)
-plot_difference(mdiff, title="Topic difference (one model) [jaccard distance]",
-                annotation=annotation)
-plt.show()
+    title = "PCA Visualization of the Dataset using {}"
+    if use_umap is True:
+        from umap import UMAP
+        dim_reducer = UMAP(n_components=2)
+        title = title.format("UMAP")
+    else:
+        from sklearn.manifold import TSNE
+        dim_reducer = TSNE(n_components=2)
+        title = title.format("TSNE")
+
+    x_transform = np.concatenate((x_train, x_test))
+    x_transform = StandardScaler().fit_transform(x_transform)
+    x_transform = dim_reducer.fit_transform(x_transform)
+
+    x2d_train = x_transform[:x_train.shape[0], :]
+    x2d_test = x_transform[x_train.shape[0]:, :]
+
+    print(x2d_test.shape)
+    dpi = 300
+    fig, axes = plt.subplots(ncols=2, figsize=(3000/dpi, 1500/dpi), dpi=dpi)
+    plot_topic_clusters(axes[0], x2d_train, y_train, labels)
+    plot_topic_clusters(axes[1], x2d_test, y_test, labels)
+    axes[0].set_title("Train Subset")
+    axes[1].set_title("Test Subset")
+    fig.suptitle(title)
+    fig.tight_layout()
+    plt.savefig("results/20newsgroup_sports_vis.png")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            prog="This is an example for visualizing twentynewsgroup "
+                 "dataset.")
+    parser.add_argument("--enable-umap", action="store_true", required=False,
+                        default=False)
+    args = parser.parse_args()
+    main(args.enable_umap)
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md") as fh:
     long_desc = fh.read()
 
+with open("requirements.txt") as req:
+    requirements = req.readlines()
+
 setuptools.setup(name="lda_classification", version="0.0.2",
                  author="Farhood Etaati", author_email="farhoodet@gmail.com",
                  long_description=long_desc,
@@ -12,8 +15,4 @@ setuptools.setup(name="lda_classification", version="0.0.2",
                  classifiers=["Programming Language :: Python :: 3",
                               "License :: OSI Approved :: MIT License",
                               "Operating System :: OS Independent"],
-                 install_requires=["gensim == 3.8.0", "matplotlib == 3.1.2",
-                                   "numpy == 1.19.1", "pandas == 1.1.0",
-                                   "scikit_learn == 0.23.1", "spacy == 2.3.1",
-                                   "tqdm == 4.48.2", ],
-                 python_requires='>=3.7.5')
+                 install_requires=requirements, python_requires='>=3.7.5')
